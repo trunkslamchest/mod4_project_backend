907e19a4849cceda429cab4d162ef859504f24e5
austin_smith <trunk9slamchest@gmail.com>
2019-12-05 17:16:41 -0500

    updated cart serializer

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,24 +1,33 @@
 class CartItemsController < ApplicationController
 
 	def index
-		@cart = CartItem.all
+		# @cart = CartItem.all
+		@cart = CartItem.order(id: :desc)
 		render json: CartItemsSerializer.new(@cart).serialized_json, include: "**"
 	end
 
 	def show
-		@cart_item = CartItem.find(params[:id])
+		@cart_item = CartItem.find(params[:id]).order(id: :desc)
 
 		render json: @cart_item
 	end
 
 	def create
-		@cart = CartItem.create(create_cart_items_params)
+		@cart = CartItem.create(cart_item_params)
 
 		if @cart.valid?
 			render json: @cart
 		else
 			render json: {errors: @cart.errors.full_messages}, status: 401
 		end
+	end
+	
+	def update
+		@cart_item = CartItem.find(params[:id])
+
+		@cart_item.update(cart_item_params)
+
+		render json: @cart_item
 	end
 
 	def destroy
@@ -31,7 +40,7 @@ class CartItemsController < ApplicationController
 
 private
 
-	def create_cart_items_params
+	def cart_item_params
 		params.permit(:user_id, :item_id, :cart_quantity)
 	end
 
