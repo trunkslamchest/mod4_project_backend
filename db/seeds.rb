94964c2e73f33a63814c178f6a2a8910493ee54a
austin_smith <trunk9slamchest@gmail.com>
2019-12-05 15:57:50 -0500

    merged with aibeck's code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,18 @@
 
-User.create(username: "user1", password: "lol1")
-User.create(username: "user2", password: "lol2")
-User.create(username: "user3", password: "lol3")
+User.create(username: "user1", password: "pw1")
+User.create(username: "user2", password: "pw2")
+User.create(username: "user3", password: "pw3")
 
-Item.create(name: 'item1', img_url: "https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct", category: "test_category1", price: 25.00, description: "test_description1", quantity: 1)
-Item.create(name: 'item2', img_url: "https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct", category: "test_category2", price: 50.00, description: "test_description2", quantity: 2)
-Item.create(name: 'item3', img_url: "https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct", category: "test_category3", price: 75.00, description: "test_description3", quantity: 3)
-Item.create(name: 'item4', img_url: "https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct", category: "test_category4", price: 100.00, description: "test_description4", quantity: 4)
-
-
+Item.create(name: 'TV', img_url: "https://content.abt.com/media/images/products/BDP_Images/big_XBR85X900F.jpg", category: "Electronics", price: "1700.00", description: "Mint condition", quantity: "2")
+Item.create(name: 'UGG', img_url: "https://www.ugg.com/dw/image/v2/AAFF_PRD/on/demandware.static/-/Sites-masterCatalogUgg/default/dw6bbc4b38/images/grey/1108231-GREY_1.jpg?sw=1500", category: "Shoes", price: "200.00", description: "very WaRm", quantity: "10")
+Item.create(name: 'Iphone 11', img_url: "https://www.apple.com/v/iphone-11-pro/c/images/meta/og__smc3haxsdn2q_overview.png", category: "Electronics", price: "900.00", description: "brand new", quantity: "5")
+Item.create(name: 'Airpods', img_url: "https://zdnet1.cbsistatic.com/hub/i/r/2019/04/02/4e6550a0-c2e9-43b6-9366-3f05cb6b53ba/resize/1200x900/88b7773f498f3541fa6277414beb8d46/airpods-2019-out-of-case.jpg", category: "Electronics", price: "100.00", description: "used", quantity: "5")
+Item.create(name: 'Iphone 7plus', img_url: "https://images-na.ssl-images-amazon.com/images/I/61CvAFp0GKL._SY445_.jpg", category: "Electronics", price: "500.00", description: "used", quantity: "5")
+Item.create(name: 'Calculator', img_url: "https://cdn11.bigcommerce.com/s-xov91f4/images/stencil/1280x1280/products/2068/2721/fx-55plus__81693.1498759930.jpg?c=2&imbypass=on", category: "Electronics", price: "40.00", description: "new", quantity: "10")
+Item.create(name: 'Macbook Pro', img_url: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-201807?wid=1808&hei=1680&fmt=jpeg&qlt=80&.v=1529520060550", category: "Electronics", price: "1440.00", description: "new", quantity: "4")
+Item.create(name: 'AirMax 100', img_url: "https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/8/0/803395_01.jpg", category: "Shoes", price: "240.00", description: "new", quantity: "4")
+Item.create(name: 'AirMax 120', img_url: "https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/8/0/801068_1.jpg", category: "Shoes", price: "140.00", description: "new", quantity: "4")
+Item.create(name: 'AirMax 90', img_url: "https://stockx-360.imgix.net/Nike-Air-Max-90-Be-True-2019/Images/Nike-Air-Max-90-Be-True-2019/Lv2/img01.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1560199460", category: "Shoes", price: "340.00", description: "new", quantity: "4")
 
 
 
